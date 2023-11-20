0d1059821a18d3269f1f09864736b8fd0c0144de
Vu Nguyen <vu.nguyen@dmttek.com>
2023-11-20 15:51:09 +0700

    clean dist

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-const { createConfig } = require('@edx/frontend-build');
-
-module.exports = createConfig('jest', {
-  setupFiles: [
-    '<rootDir>/src/setupTest.js',
-  ],
-  testTimeout: 20000,
-});
